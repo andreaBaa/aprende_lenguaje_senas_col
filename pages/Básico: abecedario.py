752a49ecb17cdc63c08ac0e9b801534b0d1dca0f
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-22 22:42:53 -0500

    Update Básico: abecedario.py

--- pages/Básico: abecedario.py
+++ pages/Básico: abecedario.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-[Ir a Básico:abecedario](https://aprendelenguajesenascol.streamlit.app/B%C3%A1sico:_abecedario)
+[Ir a módulo Básico: abecedario](https://aprendelenguajesenascol.streamlit.app/B%C3%A1sico:_abecedario)
