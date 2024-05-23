47dc74ad3dc387d35514ae2538d8a4406fc83e95
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-23 10:38:38 -0500

    Update Básico: tu propia seña.py

--- pages/Básico: tu propia seña.py
+++ pages/Básico: tu propia seña.py
@@ -1,15 +1,21 @@
+import os
 import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 from PIL import Image, ImageDraw, ImageFont
 
-st.title(" ¡Aprende lenguaje de señas colombiano!")
+# Título de la aplicación
+st.title("¡Aprende Lenguaje de Señas Colombiano!")
 
+# Descripción de la sección
 st.write("""
 ### Básico: Tu Señal de Identificación
 
 En esta sección, puedes crear tu propia señal de identificación personalizada. 
 En la comunidad de personas sordas, la presentación de los nombres se realiza de manera única y significativa a través del lenguaje de señas. 
 Este proceso no solo implica deletrear el nombre con el alfabeto manual, sino también, en muchas ocasiones, incluir un "nombre en señas". 
-Este nombre en señas, va más allá de la mera identificación, es en un reflejo de la identidad y la conexión social dentro de la comunidad.
+Este nombre en señas, va más allá de la mera identificación, es un reflejo de la identidad y la conexión social dentro de la comunidad.
 """)
 
 # Video explicativo
@@ -19,23 +25,63 @@ Mira este video para conocer más detalles sobre la señal de identificación.
 video_url = "https://www.youtube.com/watch?v=sGg6p03wADw" 
 st.video(video_url)
 
+# Sección para poner en práctica
 st.write("""
 ## ¡Ponlo en Práctica!
 Captura una característica distintiva, ya sea física, de personalidad o relacionada con una experiencia memorable y crea tu propia seña:
 """)
+st.write("Presiona el boton Comienza y di la palabra foto para activarla ")
 
-img_file_buffer = st.camera_input("Toma una Foto")
+# Configuración del botón de reconocimiento de voz
+stt_button = Button(label="Comienza", width=200, button_type="success")
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-if img_file_buffer is not None:
-    image = Image.open(img_file_buffer)
-    st.image(image, caption="Tu Señal de Identificación")
-    
-    st.download_button(
-        label="Descargar",
-        data=open("señal_identificacion.jpg", "rb").read(),
-        file_name="señal_identificacion.jpg",
-         mime="image/jpeg" 
-    )
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if (value.toLowerCase().includes("foto")) {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+"""))
+
+# Funcionalidad de captura de imagen
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result and "GET_TEXT" in result:
+    command = result.get("GET_TEXT")
+    st.write(f"Comando detectado: {command}")
+    if "foto" in command.lower():
+        img_file_buffer = st.camera_input("Toma una Foto")
+
+        if img_file_buffer is not None:
+            image = Image.open(img_file_buffer)
+            st.image(image, caption="Tu Señal de Identificación")
+
+            # Guardar imagen y agregar botón de descarga
+            image.save("señal_identificacion.jpg")
+            st.download_button(
+                label="Descargar",
+                data=open("señal_identificacion.jpg", "rb").read(),
+                file_name="señal_identificacion.jpg",
+                mime="image/jpeg"
+            )
+
+# Sección para compartir la señal
 st.write("""
 ### ¡Comparte tu Señal!
 Una vez que hayas creado tu señal de identificación, compártela con tus amigos y familiares para que puedan reconocerte fácilmente en la comunidad.
